c49ff8caa1c543b30b7b4b489d50c3d888e48a09
Cian Ormond <co@cianormond.com>
2023-12-03 20:06:15 -0800

    1-2

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,39 @@ use std::io::Read;
 use regex::Regex;
 
 fn main() {
-    let mut input = vec![];
-    let file = File::open("in/1-1.txt");
-    file.unwrap().read_to_end(&mut input).unwrap();
-
-    println!("{}", day_one_one(String::from_utf8(input).unwrap()))
+    println!("{}", day_one(true))
 }
 
-fn day_one_one(input: String) -> String {
-    let re = Regex::new(r"[0-9]").unwrap();
+fn read_input(path: &str) -> String {
+    let mut input = vec![];
+    let file = File::open(path);
+    file.unwrap().read_to_end(&mut input).unwrap();
+
+    String::from_utf8(input).unwrap()
+}
+
+fn day_one(is_part_two: bool) -> String {
+    let input = read_input("in/1-1.txt");
+    let re = Regex::new(r"[1-9]").unwrap();
+    let number_words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
     let mut numbers: Vec<i32> = vec![];
-    for line in input.split("\n").collect::<Vec<_>>() {
-        let nums: Vec<String> = re.captures_iter(line).map(|c| c[0].to_string()).collect();
+    for mut line in input.split("\n").map(|s| s.to_string()).collect::<Vec<_>>() {
+        if is_part_two {
+            let mut new = String::new();
+            for i in 0..line.len() {
+                new.push(line.as_bytes()[i] as char);
+                for ii in 0..number_words.len() {
+                    let end = i + number_words[ii].len();
+                    if line.len() <= end || &line[i..end] != number_words[ii] { continue; }
+                    new.push((ii + 1).to_string().parse().unwrap());
+                    break;
+                }
+            }
+            line = new;
+        }
+
+        let nums: Vec<String> = re.captures_iter(&line).map(|c| c[0].to_string()).collect();
         let num = format!("{}{}", nums.first().unwrap(), nums.last().unwrap());
         numbers.push(num.parse().unwrap());
     }
